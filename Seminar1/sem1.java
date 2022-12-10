96b6e52ba9ea4eba151b7162a05fe0e927dd31a0
BRATnavsegda <109758944+BRATnavsegda@users.noreply.github.com>
2022-12-10 12:27:44 +0300

    123

---
+++ Seminar1/sem1.java
@@ -0,0 +1,83 @@
+package Seminar1;
+
+public class sem1 {
+    public static void main(String[] args) {
+        
+    }
+
+    // System.out.println("Как тебя зовут?");
+    //     Scanner scanner = new Scanner(System.in);
+    //     String name = scanner.nextLine();
+
+    //     LocalTime time = LocalTime.now();
+    //     int hour = time.getHour();
+
+    //     String res = "";
+    //     if (hour >= 5 && hour < 12){
+    //         res += "Доброе утро, ";
+    //     } else if (hour >= 12 && hour < 18){
+    //         res += "Добрый день, ";
+    //     } else if (hour >= 18 && hour < 23) {
+    //         res += "Добрый вечер, ";
+    //     } else {
+    //         res += "Доброй ночи, ";
+    //     }
+
+    //     System.out.println(res + name);
+
+
+
+
+
+    // public static int ex2(int[] arr){
+    //     int maxCount = 0;
+    //     int count = 0;
+    //     for (int i = 0; i < arr.length; i++) {
+    //         if (arr[i] == 1){
+    //             count++;
+    //         } else {
+    //             if (maxCount < count){
+    //                 maxCount = count;
+    //             }
+    //             count = 0;
+    //         }
+    //     }
+    //     if (maxCount < count){
+    //         maxCount = count;
+    //     }
+    //     return maxCount;
+    // }
+        
+
+        
+    // public static String ex3(String s){
+    //     String[] arr = s.split(" ");
+    //     String res = "";
+    //     for (int i = arr.length-1; i >= 0 ; i--) {
+    //         res += arr[i] + " ";
+    //     }
+    //     return res;
+    // }
+
+    // public static double ex4(int a, int b){
+    //     if (b == 0) {
+    //         return 1;
+    //     }
+    //     if (a == 0 || a == 1){
+    //         return a;
+    //     }
+    //     double a1 = a;
+    //     if (b < 0){
+    //         b = -b;
+    //         a1 = 1 / a1;
+    //     }
+
+    //     double res = 1;
+    //     for (int i = 0; i < b; i++) {
+    //         res *= a1;
+    //     }
+    //     return res;
+    // }
+
+
+}
