e94a30f8ca2722b3e52ec0b8e96f2c8e4c0e11e5
BRATnavsegda <109758944+BRATnavsegda@users.noreply.github.com>
2022-12-14 16:23:27 +0300

    homework2

--- Seminar2/sem2/src/App.java
+++ Seminar2/sem2/src/App.java
@@ -1,8 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
+        Equation();
     }
 
     public static int GetNumber() {
@@ -23,10 +26,37 @@ public class App {
     public static String GetString() {
         Scanner str = new Scanner(System.in);
         String equation;
-        System.out.print("Введите формулу");
-        equation = str.next();
+        System.out.print("Введите формулу: ");
+        equation = str.nextLine();
         return equation;
 
     }
+
+    public static void Equation() {
+
+        Map<String, Integer> mapNumbers = new HashMap<String, Integer>();
+        mapNumbers.put("0", 0);
+        Scanner str = new Scanner(System.in);
+
+        String equation = GetString();
+        equation = String.format(equation).replace(" ", "");
+    String[] st1 = equation.split("\\+");
+    // int[] numbers = new int[st1.length];
+    int sum = 0;
+    for (int i = 0; i < st1.length; i++) {
+        for(Map.Entry<String, Integer> item : mapNumbers.entrySet()){
+            if (item.getKey() != st1[i]){
+                mapNumbers.put(st1[i], str.nextInt());
+                break;
+            }
+
+        }
+    }
+    for (Map.Entry<String, Integer> item : mapNumbers.entrySet()){
+        sum += item.getValue();
+    }
+    System.out.println(sum);
     
+    }
+
 }
